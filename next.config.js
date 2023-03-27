42ea81ca1c533e3cc6d9cd11b9fb413f107fd07b
Dan Starner <me@danstarner.com>
2023-03-27 19:38:25 -0400

    added ride for roswell redirect

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-const withMDX = require('@next/mdx')({
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
   },
-})
+});
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "md", "mdx"],
   target: "server",
+  async redirects() {
+    return [
+      {
+        source: "/roswell",
+        destination: "http://give.roswellpark.org/goto/danstarner",
+        permanent: true,
+      },
+    ];
+  },
 });
